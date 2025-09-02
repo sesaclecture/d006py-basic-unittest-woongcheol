761022ed73f966660fd78556ec02fa0d75b88ff6
woo <5starshin@gmail.com>
2025-09-02 12:08:56 +0900

    과제 제출

---
+++ unittest/basic_func.py
@@ -0,0 +1,20 @@
+def evn(num1):
+    if num1 % 2 == 0:
+        return True
+    else:
+        return False
+
+def avg(num1, num2):
+    return (num1 + num2) / 2
+
+def max(num1, num2):
+    if num1 > num2:
+        return num1
+    else:
+        return num2
+
+def min(num1, num2):
+    if num1 < num2:
+        return num1
+    else:
+        return num2
--- unittest/test_funcs.py
+++ unittest/test_funcs.py
@@ -1,6 +1,25 @@
-# TODO: 사용자 모듈 import
+from basic_func import evn, avg, max, min
 
+def test_evn():
+    assert evn(2) == True
+    assert evn(1) == False
+    assert evn(8) == True
+    assert evn(3) == False
 
-# TODO: 아래의 코드를 삭제하고 unittest를 작성하세요.
-def test_always():
-    assert False, "Remove me!"
+def test_avg():
+    assert avg(2, 4) == 3
+    assert avg(1, 5) == 3
+    assert avg(3, 7) == 5
+    assert avg(4, 8) == 6
+
+def test_max():
+    assert max(2, 4) == 4
+    assert max(1, 5) == 5
+    assert max(3, 7) == 7
+    assert max(4, 8) == 8
+
+def test_min():
+    assert min(2, 4) == 2
+    assert min(1, 5) == 1
+    assert min(3, 7) == 3
+    assert min(4, 8) == 4
